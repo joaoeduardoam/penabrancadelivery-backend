95f2651b8b1756b00f1a805b82b1436514aee4bb
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-09-03 14:31:19 -0300

    feat: Generating QR Code for PIX Charge

--- src/main/java/com/joaoeduardo/penabrancadelivery_backend/pix/PixService.java
+++ src/main/java/com/joaoeduardo/penabrancadelivery_backend/pix/PixService.java
@@ -7,7 +7,12 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PixService {
@@ -20,7 +25,7 @@ public class PixService {
 
     public JSONObject pixCreateEVP() {
 
-        JSONObject options = getJSONObjectOptions(new Credentials());
+        JSONObject options = getJSONObjectOptions();
 
         try {
             EfiPay efi = new EfiPay(options);
@@ -38,20 +43,50 @@ public class PixService {
 
     }
 
+    public void pixGenerateQRCode(String id){
+
+
+
+        JSONObject options = getJSONObjectOptions();
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("id", id);
+
+        try {
+            EfiPay efi= new EfiPay(options);
+            Map<String, Object> response = efi.call("pixGenerateQRCode", params, new HashMap<String, Object>());
+
+            System.out.println(response);
+
+            File outputfile = new File("qrCodeImage.png");
+            ImageIO.write(ImageIO.read(new ByteArrayInputStream(javax.xml.bind.DatatypeConverter.parseBase64Binary(((String) response.get("imagemQrcode")).split(",")[1]))), "png", outputfile);
+            Desktop desktop = Desktop.getDesktop();
+            desktop.open(outputfile);
+
+        }catch (EfiPayException e){
+            System.out.println(e.getError());
+            System.out.println(e.getErrorDescription());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
     public JSONObject pixCreateCharge(PixChargeRequest pixChargeRequest) throws Exception {
 
 
-        JSONObject options = getJSONObjectOptions(new Credentials());
+        JSONObject options = getJSONObjectOptions();
 
-        HashMap<String, String> params = new HashMap<String, String>();
-        params.put("txid", "7978c0c97ea847e78e8849634473c1f2");
+//        HashMap<String, String> params = new HashMap<String, String>();
+//        params.put("txid", "7978c0c97ea847e78e8849634473c1f2");
 
         JSONObject body = new JSONObject();
         body.put("calendario", new JSONObject().put("expiracao", 3600));
         body.put("devedor", new JSONObject().put("cpf", "12345678909").put("nome", "Francisco da Silva"));
         body.put("valor", new JSONObject().put("original", pixChargeRequest.value()));
         body.put("chave", pixChargeRequest.keyPix());
-        body.put("solicitacaoPagador", "Serviço realizado.");
+//        body.put("solicitacaoPagador", "Serviço realizado.");
 
         JSONArray infoAdicionais = new JSONArray();
         infoAdicionais.put(new JSONObject().put("nome", "Campo 1").put("valor", "Informação Adicional1 do PSP-Recebedor"));
@@ -61,7 +96,11 @@ public class PixService {
 
         try {
             EfiPay efi = new EfiPay(options);
-            JSONObject response = efi.call("pixCreateCharge", params, body);
+            JSONObject response = efi.call("pixCreateImmediateCharge", new HashMap<String,String>(), body);
+
+            int idFromJson= response.getJSONObject("loc").getInt("id");
+
+            pixGenerateQRCode(String.valueOf(idFromJson));
             System.out.println(response);
             return response;
         } catch (EfiPayException e) {
@@ -75,7 +114,9 @@ public class PixService {
 
     }
 
-    private JSONObject getJSONObjectOptions(Credentials credentials){
+    private JSONObject getJSONObjectOptions(){
+
+        Credentials credentials = new Credentials();
 
         JSONObject options = new JSONObject();
         options.put("client_id", clientId);
